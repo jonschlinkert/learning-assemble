745d5f2a6d93927dbaef3e90c94c665b10eabacc
James Wing <jvwing@gmail.com>
2014-10-23 13:44:37 -0700

    Layout and partial templates with configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,13 @@ module.exports = function (grunt) {
     grunt.initConfig({
         assemble: {
             hello: {
+                options: {
+                    layoutdir: 'source/templates/layouts',
+                    layout: 'default.hbs',
+                    partials: 'source/templates/partials/**/*.hbs'
+                },
                 files: [
-                    {
-                        expand: true,
-                        cwd: 'source/templates/pages',
-                        src: '**/*.hbs',
-                        dest: 'output/'
-                    }
+                    { expand: true, cwd: 'source/templates/pages', src: '**/*.hbs', dest: 'output/' }
                 ]
             }
         }
